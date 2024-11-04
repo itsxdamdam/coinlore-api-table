1be23a798dcfef89f9a7e9d12ae3458ddf03d53b
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2024-11-04 19:15:19 +0100

    feat:pagination buttons

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Pagination = () => {
+  const handleNextPage = () => {
+    if (currentPage < Math.ceil(coinList.length / itemsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if(currentPage > 1) {
+
+    }
+  }
+
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center font-bold">
       <button>
+        <FontAwesomeIcon icon={faArrowLeft} className="px-1" />
         Previous
       </button>
       <button>
         Next
+        <FontAwesomeIcon icon={faArrowRight} className="px-1" />
       </button>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
