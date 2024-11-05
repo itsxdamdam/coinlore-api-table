b49e2f403a947db86e260a1bcbb263987e66e01a
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2024-11-05 08:45:46 +0100

    fix: bg-color

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,13 +1,13 @@
 export const Coin = ({ name, symbol, price, totalSupply, index }) => (
   <tbody>
     <tr className={index % 2 ? "py-3 w-full" : "bg-neutral-300 py-5 w-full"}>
-      <td className="p-1 sm:p-3 md:text-wrap">
+      <td className="py-1 sm:p-3 text-wrap">
         <span className="block font-bold sm:hidden">💰Coin</span>
         {name}
       </td>
-      <td className="p-1 sm:p-3"><span className="block font-bold sm:hidden">📄Code</span>{symbol}</td>
-      <td className="p-1 sm:p-3"><span className="block font-bold sm:hidden">🤑 Price</span>${price}</td>
-      <td className="p-1 sm:p-3">
+      <td className="py-1 sm:p-3"><span className="block font-bold sm:hidden">📄Code</span>{symbol}</td>
+      <td className="py-1 sm:p-3"><span className="block font-bold sm:hidden">🤑 Price</span>${price}</td>
+      <td className="py-1 sm:p-3">
         <span className="block font-bold sm:hidden">📉 Total Supply</span>
         {totalSupply} {symbol}
       </td>
